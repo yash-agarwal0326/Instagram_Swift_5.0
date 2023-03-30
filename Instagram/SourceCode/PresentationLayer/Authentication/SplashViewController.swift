e65bc13868324dd0b3db19e1e40bfdc42d355a3e
Yash Agarwal <yash_agarwal@softprodigy.com>
2023-03-30 23:12:33 +0530

    splash added

---
+++ Instagram/SourceCode/PresentationLayer/Authentication/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  Instagram
+//
+//  Created by Yash Agarwal on 30/03/23.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
